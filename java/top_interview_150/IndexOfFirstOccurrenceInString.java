2e4b97b4c480f41b3bf659a902aa4279bd7694ea
SupriyaMuppu <muppusupriya@gmail.com>
2024-05-30 14:49:14 +0530

    update readme

---
+++ java/top_interview_150/IndexOfFirstOccurrenceInString.java
@@ -0,0 +1,7 @@
+package top_interview_150;
+
+public class IndexOfFirstOccurrenceInString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
